34bb4dab52421bcffbf7078b35e1e4185649d4e6
Amneh16 <53042820+Amneh16@users.noreply.github.com>
2020-08-27 14:42:44 +0300

    Add files via upload

---
+++ ServerTsofen45/src/main/java/com/example/ServerTsofen45/ServerTsofen45Application.java
@@ -0,0 +1,13 @@
+package com.example.ServerTsofen45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTsofen45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerTsofen45Application.class, args);
+	}
+
+}
