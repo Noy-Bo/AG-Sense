bdcaed516e0f57a5d094c20349fe81ba2cd63a61
anan-amara <58443636+anan-amara@users.noreply.github.com>
2020-08-30 18:58:46 +0300

    #not_Important

--- ServerTsofen45/src/main/java/com/example/ServerTsofen45/Beans/Device.java
+++ ServerTsofen45/src/main/java/com/example/ServerTsofen45/Beans/Device.java
@@ -1,12 +1,23 @@
 package com.example.ServerTsofen45.Beans;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity(name="devices")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class Device {
+	int id;
+	@Id
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 
 	
 }
