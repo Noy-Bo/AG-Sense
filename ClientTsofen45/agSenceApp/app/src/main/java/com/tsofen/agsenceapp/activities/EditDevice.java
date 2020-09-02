468929af97bf0b550886131f070fec8fd8201cc4
ameer122 <ameerkadi97@gmail.com>
2020-09-02 14:47:27 +0300

    Added the following activitys : activity_account_status_news_shape.xml activity_accountstatusfilter.xml activity_device_settings.xml activity_edit_account.xml activity_edit_device.xml activity_health_care_dashboard.xml activity_new_account.xml activity_new_device.xml
    Also few changes has been made to strings.xml, styles.xml

---
+++ ClientTsofen45/agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/EditDevice.java
@@ -0,0 +1,17 @@
+package com.tsofen.agsenceapp.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.tsofen.agsenceapp.R;
+
+public class EditDevice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_device);
+
+    }
+}
