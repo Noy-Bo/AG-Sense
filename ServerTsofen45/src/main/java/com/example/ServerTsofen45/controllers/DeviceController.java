66d4518864e5a33c4f274b93171ba16bf561f052
Amneh Hassan <amneh.hassan@gmail.com>
2020-09-03 15:43:14 +0300

    changesInDeviceAndDeviceData

--- ServerTsofen45/src/main/java/com/example/ServerTsofen45/controllers/DeviceController.java
+++ ServerTsofen45/src/main/java/com/example/ServerTsofen45/controllers/DeviceController.java
@@ -1,10 +1,31 @@
 package com.example.ServerTsofen45.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ServerTsofen45.BL.DeviceBL;
+import com.example.ServerTsofen45.Beans.Device;
 
 @RestController
 @RequestMapping("Device")
 public class DeviceController {
+
+	@Autowired
+	DeviceBL deviceBL;
+
+	@GetMapping("DeviceById")
+	public Device getSpicificDeviceById(@RequestParam int id, @RequestParam int imei) {
+
+		// get device from device logic
+
+		Device device = deviceBL.getSpecificDeviceById(id, imei);
+		if (device == null)
+			return null;
+		return device;
+
+	}
 
 }
