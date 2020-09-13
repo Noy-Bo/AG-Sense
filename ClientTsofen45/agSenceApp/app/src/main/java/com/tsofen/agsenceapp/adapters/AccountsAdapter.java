57596871579b3d5a0151f7cff99fd763be3ce5d9
ameer122 <ameerkadi97@gmail.com>
2020-09-13 15:37:55 +0300

    fixed account_status img , added Setting.java

--- ClientTsofen45/agSenceApp/app/src/main/java/com/tsofen/agsenceapp/adapters/AccountsAdapter.java
+++ ClientTsofen45/agSenceApp/app/src/main/java/com/tsofen/agsenceapp/adapters/AccountsAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.tsofen.agsenceapp.R;
@@ -31,7 +32,8 @@ public class AccountsAdapter extends ArrayAdapter<User> implements Serializable 
         TextView name = layout.findViewById(R.id.accountname);
         TextView amountofdevices = layout.findViewById((R.id.amountofdevices));
         TextView accountlastupdate = layout.findViewById((R.id.accountlastupdate));
-
+        ImageView imageView = layout.findViewById(R.id.account_status_imageview);
+        imageView.setImageResource(R.drawable.faulty_accounts_icon);
 
         name.setText(account.getUsername());
         return layout;
