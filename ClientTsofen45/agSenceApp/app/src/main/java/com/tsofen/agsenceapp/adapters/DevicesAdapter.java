389bb3aae399514c559e13e15bb89829ecef689c
Abed <id.abed@gmail.com>
2020-09-21 11:14:19 +0300

    added linking from admin to device view and from admin to account dashbard when clicking on listview

--- ClientTsofen45/agSenceApp/app/src/main/java/com/tsofen/agsenceapp/adapters/DevicesAdapter.java
+++ ClientTsofen45/agSenceApp/app/src/main/java/com/tsofen/agsenceapp/adapters/DevicesAdapter.java
@@ -1,14 +1,19 @@
 package com.tsofen.agsenceapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tsofen.agsenceapp.R;
+import com.tsofen.agsenceapp.activities.AccountDashboardActivity;
+import com.tsofen.agsenceapp.activities.AppBaseActivity;
+import com.tsofen.agsenceapp.activities.DeviceView;
 import com.tsofen.agsenceapp.entities.Devices;
 
 import java.io.Serializable;
@@ -29,7 +34,7 @@ public class DevicesAdapter extends ArrayAdapter<Devices> implements Serializabl
     public View getView(int position, View convertView, ViewGroup parent) {
 
         View layout = this.inflater.inflate(R.layout.activity_device_status_shape, null);
-        Devices devices = getItem(position);
+        final Devices devices = getItem(position);
         TextView name = layout.findViewById(R.id.accountname);
         TextView devicetypeid = layout.findViewById((R.id.deviceidtype));
         TextView faultytime = layout.findViewById((R.id.devicefaultTime));
@@ -43,6 +48,16 @@ public class DevicesAdapter extends ArrayAdapter<Devices> implements Serializabl
         faultytime.setText(String.valueOf(devices.getFaultTime()));
         lastupdate.setText(String.valueOf(devices.getLastUpdate()));
 
+
+        LinearLayout linearLayout = layout.findViewById(R.id.DeviceItemShape);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), DeviceView.class);
+                intent.putExtra("device", devices);
+                getContext().startActivity(intent);
+            }
+        });
         return layout;
     }
 }
