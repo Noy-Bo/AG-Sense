6642e142858e64bef953dc031ce9353bb3dec1dd
niryakub <niryakub@gmail.com>
2020-09-23 13:37:18 +0300

    "fixes made

--- ClientTsofen45/agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/DeviceView.java
+++ ClientTsofen45/agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/DeviceView.java
@@ -20,6 +20,7 @@ import com.tsofen.agsenceapp.dataAdapters.DeviceDataAdapter;
 import com.tsofen.agsenceapp.entities.DeviceData;
 import com.tsofen.agsenceapp.entities.Devices;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DeviceView extends AppBaseActivity {
@@ -40,11 +41,6 @@ public class DeviceView extends AppBaseActivity {
 
         sliderViewPager = (ViewPager) findViewById(R.id.viewPager);
         dotslinearLayout = (LinearLayout) findViewById(R.id.sliderDotsLayout);
-        sliderAdapter = new SliderAdapter(this);
-
-        //applying the adapter onto the viewpager!
-        sliderViewPager.setAdapter(sliderAdapter);
-        addDotsIndicator(0);
 
         sliderViewPager.addOnPageChangeListener(viewListener);
         device = (Devices) getIntent().getSerializableExtra("device");
@@ -53,7 +49,7 @@ public class DeviceView extends AppBaseActivity {
 
         DeviceDataAdapter.getInstance().getDeviceDataList(device.getId(), new DeviceInfoDataRequestHandler() {
             @SuppressLint("DefaultLocale")
-            @Override
+            @Override //applying logic to the handler once Data is received by the thread...
             public void getDeviceDataInfo(List<DeviceData> deviceDataList) {
                 TextView status = findViewById(R.id.device_view_status);
                 TextView lastUpdate = findViewById(R.id.device_view_last_update);
@@ -65,13 +61,26 @@ public class DeviceView extends AppBaseActivity {
                     coordinations.setText("Lat: ---- Long: ---- "); // no height
                     isMoving.setText("Moving: ----");
                     return;
-                }
-                DeviceData deviceData = deviceDataList.get(0);
+                } /////
+                final  DeviceData deviceData = deviceDataList.get(0);
                 device.setDeviceData(deviceDataList);
                 status.setText(String.format("Device Status: %s", device.getFaulty() ? "faulty" : "healthy"));
                 lastUpdate.setText("last updated: " + device.getLastUpdate());
                 coordinations.setText(String.format("Lat: %f Long: %f ", deviceData.getLat(), deviceData.getLon())); // no height
                 isMoving.setText(String.format("Moving: %s", ((deviceData.getMoveAlertActive()) ? "Yes" : "No")));
+
+                //----------
+                DeviceView.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        sliderAdapter = new SliderAdapter(DeviceView.this, deviceData);
+                        //applying the adapter onto the viewpager!
+                        sliderViewPager.setAdapter(sliderAdapter);
+                        addDotsIndicator(0);
+                    }
+                });
+
+
             }
         });
 
@@ -79,7 +88,7 @@ public class DeviceView extends AppBaseActivity {
 
     //for indentifying the current-dot (in the dot-scroller) we're positioned on..
     public void addDotsIndicator(int position) {
-        mDots = new TextView[3];
+        mDots = new TextView[10]; //TO CHANGE
         dotslinearLayout.removeAllViews();
 
         for (int i = 0; i < mDots.length; i++) {
