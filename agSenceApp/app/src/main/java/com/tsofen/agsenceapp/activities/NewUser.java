0b80db0ec8eb7484738dd00bea0a5633c35b2ef8
ayat-taha <57218332+ayat-taha@users.noreply.github.com>
2020-09-23 15:07:13 +0300

    new user

--- agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/NewUser.java
+++ agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/NewUser.java
@@ -3,6 +3,10 @@ package com.tsofen.agsenceapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.tsofen.agsenceapp.R;
 
 public class NewUser extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class NewUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        String[] users = { "Admin", "Account", "Support"};
+        Spinner spin = (Spinner) findViewById(R.id.UserType);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
     }
 }
