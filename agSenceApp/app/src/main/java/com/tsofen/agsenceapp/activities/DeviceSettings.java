2467942849c73607830888745ef96b78191fd6a9
Rawi <rawi.x.boy@hotmail.com>
2020-09-28 11:37:52 +0300

    working on device settings activity

--- agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/DeviceSettings.java
+++ agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/DeviceSettings.java
@@ -24,8 +24,8 @@ public class DeviceSettings extends BackBaseActivity {
 
     Devices device;
     Spinner spinner;
-    int flag=0;
-
+    int flag = 0;
+    ArrayAdapter<String> dataAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -36,12 +36,13 @@ public class DeviceSettings extends BackBaseActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                if (flag==0){
-                    flag=1;
-                }else{
-                    b1.setVisibility(VISIBLE);
+                if (flag == 0) {
+                    flag = 1;
+                } else {
+                    b1.setVisibility(View.VISIBLE);
                 }
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
                 b1.setVisibility(View.INVISIBLE);
@@ -50,29 +51,31 @@ public class DeviceSettings extends BackBaseActivity {
         });
 
 
-
-
         final ArrayList<String> list = new ArrayList<>();
         device = (Devices) getIntent().getSerializableExtra("device");
-        if (device != null)
+        if (device != null) {
             list.add(device.getImei().toString());
+            dataAdapter = new ArrayAdapter<>(DeviceSettings.this, android.R.layout.simple_spinner_item, list);
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(dataAdapter);
+        }
         else {
             DeviceDataAdapter.getInstance().getAllDevices(0, 0, new DeviceDataRequestHandler() {
                 @Override
-                public void onDeviceDataLoaded(List<Devices> devices) {
+                public void onDeviceDataLoaded(final List<Devices> devices) {
                     for (Devices devices1 : devices)
                         list.add(devices1.getImei().toString());
-
-
+                    dataAdapter = new ArrayAdapter<>(DeviceSettings.this, android.R.layout.simple_spinner_item, list);
+                    dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    DeviceSettings.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            spinner.setAdapter(dataAdapter);
+                        }
+                    });
                 }
             });
         }
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(dataAdapter);
-
-
     }
 
     public Spinner getSpinner() {
