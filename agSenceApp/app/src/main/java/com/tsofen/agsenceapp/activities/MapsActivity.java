c5bd5a4e184a47b42f8d447de3e78a0a97ebc0e4
Noyb <noy.botboul@gmail.com>
2020-09-29 14:20:15 +0300

    maps with search in develop

--- agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/MapsActivity.java
+++ agSenceApp/app/src/main/java/com/tsofen/agsenceapp/activities/MapsActivity.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 
-import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,24 +19,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterItem;
 import com.google.maps.android.clustering.ClusterManager;
-import com.google.maps.android.clustering.view.ClusterRenderer;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 import com.tsofen.agsenceapp.R;
-import com.tsofen.agsenceapp.entities.DeviceData;
+import com.tsofen.agsenceapp.adapters.DevicesAdapter;
+import com.tsofen.agsenceapp.adaptersInterfaces.DeviceDataRequestHandler;
+import com.tsofen.agsenceapp.dataAdapters.DeviceDataAdapter;
 import com.tsofen.agsenceapp.entities.Devices;
 import com.tsofen.agsenceapp.entities.Place;
-import com.tsofen.agsenceapp.entities.User;
 import com.tsofen.agsenceapp.entities.UserMap;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         ClusterManager.OnClusterItemClickListener<Place>,
@@ -44,16 +43,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ClusterManager<Place> mClusterManager;
     private Renderer mRenderer;
     private LatLngBounds.Builder builder = new LatLngBounds.Builder();
+    private ConstraintLayout mMapLayout;
+    private AutoCompleteTextView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps_with_search);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         userMap = (UserMap) getIntent().getExtras().getSerializable("user_map");
+
+
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View contentView = inflater.inflate(R.layout.activity_device_status, null, false);
+
+        // only if we are at map for all devices
+            searchView = (AutoCompleteTextView) findViewById(R.id.map_search_text_view);
+            searchView.setHint(R.string.search_device_hint);
+            DeviceDataAdapter.getInstance().getAllDevices(0, 0, new DeviceDataRequestHandler() {
+                @Override
+                public void onDeviceDataLoaded(final List<Devices> devices) {
+                    MapsActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            searchView.setAdapter(new DevicesAdapter<Devices>(MapsActivity.this, devices));
+                        }
+                    });
+
+                }
+            });
+            searchView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent intent = new Intent(MapsActivity.this, DeviceView.class);// write here method that zooms at specific device when chosen. via lat/long maybe?
+                    Devices device = (Devices) searchView.getAdapter().getItem(i);
+                    intent.putExtra("device", device);
+                    startActivity(intent);
+                }
+            });
+
+
+
+
     }
     /**
      * Manipulates the map once available.
@@ -112,6 +146,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             });
         }
+
     }
 
     private void addPolylines() {
