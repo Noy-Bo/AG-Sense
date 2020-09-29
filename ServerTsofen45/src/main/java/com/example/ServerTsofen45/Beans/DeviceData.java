b37b8bbb508d742d0d9f84944bf1d8752570ea0d
anan-amara <58443636+anan-amara@users.noreply.github.com>
2020-09-29 14:32:05 +0300

    -setpass,changepass,addNewUser added.

--- ServerTsofen45/src/main/java/com/example/ServerTsofen45/Beans/DeviceData.java
+++ ServerTsofen45/src/main/java/com/example/ServerTsofen45/Beans/DeviceData.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity(name = "DevicesData")
+@Entity(name = "DeviceData")
 public class DeviceData {
 
 	long ID;
