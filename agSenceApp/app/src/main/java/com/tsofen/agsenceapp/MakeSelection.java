48129d4e950780e67d40251398735526eb9b254e
ameer122 <ameerkadi97@gmail.com>
2020-09-30 11:47:42 +0300

    Forget Password Ver1 -- Not done yet waiting for server

--- agSenceApp/app/src/main/java/com/tsofen/agsenceapp/MakeSelection.java
+++ agSenceApp/app/src/main/java/com/tsofen/agsenceapp/MakeSelection.java
@@ -3,15 +3,25 @@ package com.tsofen.agsenceapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MakeSelection extends AppCompatActivity {
+import com.tsofen.agsenceapp.entities.Account;
 
+public class MakeSelection extends AppCompatActivity {
+protected TextView mail_des;
+protected Account account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_selection);
+        mail_des = findViewById(R.id.mail_des);
+
+        account = (Account) getIntent().getSerializableExtra("accounts");
+        assert account != null;
+        mail_des.setText(account.getEmail());
+
     }
 
     public void callBackScreenFromMakeSelection(View view) {
@@ -26,7 +36,7 @@ public class MakeSelection extends AppCompatActivity {
 
     public void GoToEmailSending(View view) {
         Intent intent = new Intent(this, VerifyOTP.class);
-
+        intent.putExtra("account",account);
         startActivity(intent);
     }
 }
