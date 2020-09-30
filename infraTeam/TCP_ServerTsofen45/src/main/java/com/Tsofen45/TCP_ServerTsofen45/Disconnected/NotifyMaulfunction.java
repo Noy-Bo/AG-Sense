9a0f0b9ac50593422a12f8db8bc372dfe6265c93
karamabualhija <karamabualhija@gmail.com>
2020-09-30 17:12:38 +0300

    malufinction update lock

--- infraTeam/TCP_ServerTsofen45/src/main/java/com/Tsofen45/TCP_ServerTsofen45/Disconnected/NotifyMaulfunction.java
+++ infraTeam/TCP_ServerTsofen45/src/main/java/com/Tsofen45/TCP_ServerTsofen45/Disconnected/NotifyMaulfunction.java
@@ -5,18 +5,17 @@ import java.util.ArrayList;
 
 public class NotifyMaulfunction implements Runnable  {
     private static String current_imei = null;
-    private ReentrantReadWriteLock  lock;
+    final private Object lock= new Object();
     public  NotifyMaulfunction(String imei){
         current_imei = imei;
-        lock= new ReentrantReadWriteLock();
+
     }
 
     @Override
     public void run() {
     	    
-    	try
-    	{
-    		this.lock.writeLock().lock();
+
+        synchronized (lock){
             ArrayList<String> arr_ = ReportTimer.getImei_list().get(current_imei).getListHead();
             int index_todelete = ReportTimer.getImei_list().get(current_imei).getIndex();
             String curr_node = arr_.get(ReportTimer.getImei_list().get(current_imei).getIndex());
@@ -24,9 +23,6 @@ public class NotifyMaulfunction implements Runnable  {
             ReportTimer.getLists_()[0].add(curr_node);
             ReportTimer.getImei_list().put(current_imei,new NodeIndex(ReportTimer.getLists_()[0],ReportTimer.getLists_()[0].size()-1));
     	}
-    	finally
-    	{
-    		this.lock.writeLock().unlock();
-    	}
+
     }
 }
