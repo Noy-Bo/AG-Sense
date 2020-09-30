00a8817bf008173d4249541519090ba3e6c0957d
yousfatyee <yousfatyee@gmail.com>
2020-09-30 12:31:56 +0300

    reportTask component

--- infraTeam/TCP_ServerTsofen45/src/main/java/com/Tsofen45/TCP_ServerTsofen45/TcpServerTsofen45Application.java
+++ infraTeam/TCP_ServerTsofen45/src/main/java/com/Tsofen45/TCP_ServerTsofen45/TcpServerTsofen45Application.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.Tsofen45.TCP_ServerTsofen45.Disconnected.ReportTimer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -19,14 +20,16 @@ public class TcpServerTsofen45Application {
 	static int port = 1025;
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(TcpServerTsofen45Application.class, args);
-
-
+		ReportTimer reportTimer = context.getBean(ReportTimer.class);
+		reportTimer.load_imei();
+		reportTimer.start_tasks();
 		//The purpose of this class is to make thread for each message that is recieved from the device 
 		try {
 
 			@SuppressWarnings("resource")
 			ServerSocket serverSocket = new ServerSocket(port);
 			System.out.println("Server is up");
+
 			while(true)
 			{
 				//Open a socket for new device
